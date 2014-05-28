13b338066228f0e5c7f4073710aaa0502ec08aa3
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-05-28 21:52:36 +0300

    add a small json example
    should look into mongo connection now

--- handler/offers_handler.go
+++ handler/offers_handler.go
@@ -1,11 +1,25 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"net/http"
 )
 
+type Response map[string]interface{}
+
+func (r Response) String() (s string) {
+	b, err := json.Marshal(r)
+	if err != nil {
+		s = ""
+		return
+	}
+	s = string(b)
+	return
+}
+
 func Offers(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, Response{"response": "Hello, %s!"}.String(), html.EscapeString(r.URL.Path))
 }
