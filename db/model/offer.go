83d27ab905f49391c7b1655c22964754fe26195e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-05-31 12:40:21 +0300

    use pointers for model references

--- db/model/offer.go
+++ db/model/offer.go
@@ -8,12 +8,12 @@ import (
 type (
 	Offer struct {
 		Id          bson.ObjectId `json:"_id"           bson:"_id"`
-		Restaurant  Restaurant    `json:"restaurant"    bson:"restaurant"`
+		Restaurant  *Restaurant   `json:"restaurant"    bson:"restaurant"`
 		Title       string        `json:"title"         bson:"title"`
 		FromTime    time.Time     `json:"fromTime"      bson:"fromTime"`
 		ToTime      time.Time     `json:"toTime"        bson:"toTime"`
 		Description string        `json:"description"   bson:"description"`
 		Price       int           `json:"price"         bson:"price"`
-		Tags        []Tag         `json:"tags"          bson:"tags"`
+		Tags        []*Tag        `json:"tags"          bson:"tags"`
 	}
 )
