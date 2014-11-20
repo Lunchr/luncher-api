0cea715fbc07ed49a564f8126296221bab1b0308
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-11-20 23:29:09 +0200

    add offers collection DB test

--- db/offers.go
+++ db/offers.go
@@ -1,8 +1,11 @@
 package db
 
 import (
+	"time"
+
 	"github.com/deiwin/praad-api/db/model"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Offers struct {
@@ -14,6 +17,20 @@ func NewOffers(client *Client) *Offers {
 	return &Offers{collection}
 }
 
-func (offers *Offers) Insert(offersToInsert ...*model.Offer) (err error) {
-	return offers.c.Insert(offersToInsert)
+func (collection Offers) Insert(offersToInsert ...*model.Offer) (err error) {
+	docs := make([]interface{}, len(offersToInsert))
+	for i, offer := range offersToInsert {
+		docs[i] = offer
+	}
+	return collection.c.Insert(docs...)
+}
+
+func (collection Offers) GetForDate(date time.Time) (offers []*model.Offer, err error) {
+	err = collection.c.Find(bson.M{
+		"fromTime": bson.M{
+			"$gte": date,
+			"$lt":  date.AddDate(0, 0, 1),
+		},
+	}).All(&offers)
+	return
 }
---
+++ db/offers_test.go
@@ -0,0 +1,21 @@
+package db_test
+
+import (
+	"time"
+
+	// . "github.com/deiwin/praad-api/db"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Offers", func() {
+
+	It("should get offers for date", func(done Done) {
+		defer close(done)
+		date := time.Date(2014, 11, 10, 0, 0, 0, 0, time.UTC)
+		offers, err := offersCollection.GetForDate(date)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(offers)).To(Equal(2))
+	})
+})
