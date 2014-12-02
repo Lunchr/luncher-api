b0552eff5dcb644952c3384229ac80c3e8ded30b
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-12-02 23:11:13 +0200

    add session manager
    Now we can have a session id for each user session

---
+++ session/manager.go
@@ -0,0 +1,49 @@
+package session
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"net/http"
+)
+
+const sessionCookieName = "luncher_session"
+
+// Manager for the current session
+type Manager interface {
+	// GetOrInitSession returns the current session ID stored in the request
+	// as a cookie or creates a new id and writes it into the response as a cookie
+	GetOrInitSession(http.ResponseWriter, *http.Request) string
+}
+
+type manager struct{}
+
+// NewManager returns an implementation of the Manager interface
+func NewManager() Manager {
+	return &manager{}
+}
+
+func (mgr manager) GetOrInitSession(w http.ResponseWriter, r *http.Request) (session string) {
+	if cookie, err := r.Cookie(sessionCookieName); err == nil && cookie.Value != "" {
+		session = cookie.Value
+	} else {
+		session = createNewSession()
+		cookie := &http.Cookie{
+			Name:  sessionCookieName,
+			Value: session,
+			// no MaxAge because we want this to be a session cookie
+		}
+		http.SetCookie(w, cookie)
+	}
+	return
+}
+
+// createNewSession creates a new random session ID string
+func createNewSession() string {
+	// 16 bytes should be quite enough
+	b := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, b); err != nil {
+		panic("We ran out of random!?")
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
---
+++ session/manager_test.go
@@ -0,0 +1,61 @@
+package session_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	. "github.com/deiwin/praad-api/session"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	sessionCookieName = "luncher_session"
+	cookieHeader      = "Set-Cookie"
+)
+
+var _ = Describe("Manager", func() {
+	Describe("GetOrInitSession", func() {
+		var manager Manager
+
+		BeforeEach(func() {
+			manager = NewManager()
+		})
+
+		It("shoud return a non-empty string", func() {
+			cookieValue := manager.GetOrInitSession(responseRecorder, request)
+			Expect(cookieValue).NotTo(HaveLen(0))
+			verifySingleSessionCookie(responseRecorder, func(cookieValue string) {
+				Expect(cookieValue).NotTo(HaveLen(0))
+			})
+		})
+
+		Context("with session cookie in request", func() {
+			var requestCookieValue = "k_bV590l1T7mkhmwQgAIDA=="
+
+			BeforeEach(func() {
+				cookie := &http.Cookie{
+					Name:  sessionCookieName,
+					Value: requestCookieValue,
+				}
+				request.AddCookie(cookie)
+			})
+
+			It("should return the same cookie", func() {
+				cookieValue := manager.GetOrInitSession(responseRecorder, request)
+				Expect(cookieValue).To(Equal(requestCookieValue))
+			})
+		})
+	})
+})
+
+func verifySingleSessionCookie(responseRecorder *httptest.ResponseRecorder, verify func(string)) {
+	cookies := responseRecorder.HeaderMap[cookieHeader]
+	Expect(cookies).To(HaveLen(1))
+	cookie := cookies[0]
+	Expect(cookie).To(HavePrefix(sessionCookieName + "="))
+	cookieValue := strings.TrimPrefix(cookie, sessionCookieName+"=")
+	verify(cookieValue)
+}
