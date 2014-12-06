f331282522aa82fa58ce02dceee9ec047ce70969
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-12-06 14:31:16 +0200

    updates for facebook auth

--- facebook/facebook.go
+++ facebook/facebook.go
@@ -8,9 +8,26 @@ import (
 
 var domain = "haha" // XXX should this be in the FB conf?
 
-func something(conf Config, session string) string {
+// Authenticator provides the authentication functionality for Facebook users
+// using Facebook's OAuth
+type Authenticator interface {
+	// AuthURL returns a Facebook URL the user should be redirect to. The user
+	// will then be asked to log in by Facebook at that URL and will be redirected
+	// back to our API by Facebook.
+	AuthURL(session string) string
+}
+
+func NewAuthenticator(conf Config) Authenticator {
+	return authenticator{conf}
+}
+
+type authenticator struct {
+	conf Config
+}
+
+func (auth authenticator) AuthURL(session string) string {
 	opts, err := oauth2.New(
-		oauth2.Client(conf.AppID, conf.AppSecret),
+		oauth2.Client(auth.conf.AppID, auth.conf.AppSecret),
 		oauth2.RedirectURL(domain+"api/v1/oauth/facebook/redirect"),
 		oauth2.Scope("manage_pages", "publish_actions"),
 		oauth2.Endpoint(
