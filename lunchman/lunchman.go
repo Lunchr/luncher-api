bc82719d4a583a3223c59087f2c22bb2402357cd
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-03-06 21:50:34 +0200

    add methods to lunchman for creating an interactive CLI app

--- lunchman/lunchman.go
+++ lunchman/lunchman.go
@@ -1,26 +1,79 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"gopkg.in/alecthomas/kingpin.v1"
 )
 
 var (
-	lunchman = kingpin.New("lunchman", "An administrative tool to manage your luncher instance")
-	add      = lunchman.Command("add", "Add a new value to the DB")
-
-	addRegion = add.Command("region", "Add a region")
-
+	lunchman      = kingpin.New("lunchman", "An administrative tool to manage your luncher instance")
+	add           = lunchman.Command("add", "Add a new value to the DB")
+	addRegion     = add.Command("region", "Add a region")
 	addRestaurant = add.Command("restaurant", "Add a restarant")
+
+	errCanceled = errors.New("Command aborted")
+
+	checkNotEmpty = func(i string) error {
+		if i == "" {
+			return errors.New("Can't be empty!")
+		}
+		return nil
+	}
+	checkSingleArg = func(i string) error {
+		if strings.Contains(i, " ") {
+			return errors.New("Expecting a single argument")
+		}
+		return nil
+	}
 )
 
 func main() {
 	switch kingpin.MustParse(lunchman.Parse(os.Args[1:])) {
 	case addRegion.FullCommand():
-		fmt.Println("add region!")
+		name := getInputAndRetry("Please enter a name for the new region", checkNotEmpty, checkSingleArg)
+		fmt.Println("name: " + name)
 	case addRestaurant.FullCommand():
 		fmt.Println("add restaurant!")
 	}
+}
+
+func getInputAndRetry(message string, checks ...func(string) error) string {
+	for {
+		input, err := getInput(message, checks...)
+		if err != nil {
+			fmt.Printf("%v\nDo you want to try again? [y/N]: ", err)
+			rd := bufio.NewReader(os.Stdin)
+			if line, err := rd.ReadString('\n'); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else if strings.TrimSpace(line) != "y" {
+				fmt.Println(errCanceled)
+				os.Exit(1)
+			}
+			continue
+		}
+		return input
+	}
+}
+
+func getInput(message string, checks ...func(string) error) (string, error) {
+	fmt.Print(message + ": ")
+	rd := bufio.NewReader(os.Stdin)
+	line, err := rd.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	input := strings.TrimSpace(line)
+	for _, check := range checks {
+		err = check(input)
+		if err != nil {
+			return "", err
+		}
+	}
+	return input, nil
 }
