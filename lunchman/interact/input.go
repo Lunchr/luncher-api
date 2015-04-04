0cf0081b2cc9197dd0404d327b0b3fd8d4e22831
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-04-04 22:57:20 +0300

    add option to provide a default value for user input in CLI

--- lunchman/interact/input.go
+++ lunchman/interact/input.go
@@ -21,12 +21,23 @@ func (a Actor) GetInputAndRetry(message string, checks ...InputCheck) (string, e
 	for {
 		input, err := a.GetInput(message, checks...)
 		if err != nil {
-			retryMessage := fmt.Sprintf("%v\nDo you want to try again?", err)
-			confirmed, err := a.Confirm(retryMessage, ConfirmDefaultToNo)
-			if err != nil {
+			if err = a.confirmRetry(err); err != nil {
 				return "", err
-			} else if !confirmed {
-				return "", errCanceled
+			}
+			continue
+		}
+		return input, nil
+	}
+}
+
+// GetInputWithDefaultAndRetry works exactly like GetInputAndRetry, but also has
+// a default option which will be used instead if the user simply presses enter.
+func (a Actor) GetInputWithDefaultAndRetry(message, fallback string, checks ...InputCheck) (string, error) {
+	for {
+		input, err := a.GetInputWithDefault(message, fallback, checks...)
+		if err != nil {
+			if err = a.confirmRetry(err); err != nil {
+				return "", err
 			}
 			continue
 		}
@@ -37,17 +48,60 @@ func (a Actor) GetInputAndRetry(message string, checks ...InputCheck) (string, e
 // GetInput asks the user for input and performs the list of added checks on the
 // provided input. If any of the checks fail, the error will be returned.
 func (a Actor) GetInput(message string, checks ...InputCheck) (string, error) {
-	fmt.Fprint(a.w, message+": ")
+	input, err := a.getInput(message + ": ")
+	if err != nil {
+		return "", err
+	}
+	err = runChecks(input, checks...)
+	if err != nil {
+		return "", err
+	}
+	return input, nil
+}
+
+// GetInputWithDefault works exactly like GetInput, but also has a default option
+// which will be used instead if the user simply presses enter.
+func (a Actor) GetInputWithDefault(message, fallback string, checks ...InputCheck) (string, error) {
+	input, err := a.getInput(fmt.Sprintf("%s: (%s) ", message, fallback))
+	if err != nil {
+		return "", err
+	}
+	if input == "" {
+		return fallback, nil
+	}
+	err = runChecks(input, checks...)
+	if err != nil {
+		return "", err
+	}
+	return input, nil
+}
+
+func (a Actor) confirmRetry(err error) error {
+	retryMessage := fmt.Sprintf("%v\nDo you want to try again?", err)
+	confirmed, err := a.Confirm(retryMessage, ConfirmDefaultToNo)
+	if err != nil {
+		return err
+	} else if !confirmed {
+		return errCanceled
+	}
+	return nil
+}
+
+func (a Actor) getInput(prompt string) (string, error) {
+	fmt.Fprint(a.w, prompt)
 	line, err := a.rd.ReadString('\n')
 	if err != nil {
 		return "", err
 	}
-	input := strings.TrimSpace(line)
+	return strings.TrimSpace(line), nil
+}
+
+func runChecks(input string, checks ...InputCheck) error {
 	for _, check := range checks {
-		err = check(input)
+		err := check(input)
 		if err != nil {
-			return "", err
+			return err
 		}
 	}
-	return input, nil
+	return nil
 }
--- lunchman/interact/input_test.go
+++ lunchman/interact/input_test.go
@@ -147,4 +147,168 @@ var _ = Describe("Input", func() {
 		})
 	})
 
+	Describe("GetInputWithDefault", func() {
+		var def = "default value"
+		Context("without any checks", func() {
+			Context("with a simple input", func() {
+				BeforeEach(func() {
+					userInput = " user-input \n"
+				})
+
+				It("should return the trimmed input", func() {
+					input, err := actor.GetInputWithDefault(message, def)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(input).To(Equal("user-input"))
+					Eventually(output).Should(gbytes.Say(`Please answer: `))
+				})
+			})
+
+			Context("with no input (just enter)", func() {
+				BeforeEach(func() {
+					userInput = "\n"
+				})
+
+				It("should return the default value", func() {
+					input, err := actor.GetInputWithDefault(message, def)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(input).To(Equal(def))
+					Eventually(output).Should(gbytes.Say(`Please answer: \(default value\) `))
+				})
+			})
+
+			Context("with just whitespace as input", func() {
+				BeforeEach(func() {
+					userInput = " 	\n"
+				})
+
+				It("should return the default value", func() {
+					input, err := actor.GetInputWithDefault(message, def)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(input).To(Equal(def))
+					Eventually(output).Should(gbytes.Say(`Please answer: \(default value\) `))
+				})
+			})
+		})
+
+		Context("with a check", func() {
+			var (
+				checkErr error
+				check    interact.InputCheck
+			)
+			JustBeforeEach(func() {
+				check = func(input string) error {
+					Expect(input).To(Equal("user-input"))
+					return checkErr
+				}
+			})
+
+			Context("with no input (just enter), but a failing check", func() {
+				BeforeEach(func() {
+					userInput = "\n"
+					checkErr = errors.New("Check failed!")
+				})
+
+				It("should return the default value (not run the check)", func() {
+					input, err := actor.GetInputWithDefault(message, def, check)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(input).To(Equal(def))
+					Eventually(output).Should(gbytes.Say(`Please answer: \(default value\) `))
+				})
+			})
+
+			Context("with a simple input", func() {
+
+				BeforeEach(func() {
+					userInput = "user-input \n"
+				})
+
+				Context("with a failing check", func() {
+					BeforeEach(func() {
+						checkErr = errors.New("Check failed!")
+					})
+
+					It("should return the error from the check", func() {
+						_, err := actor.GetInputWithDefault(message, def, check)
+						Expect(err).To(Equal(checkErr))
+					})
+
+					Context("with another check after the failed one", func() {
+						It("should not call the second check", func() {
+							actor.GetInputWithDefault(message, def, check, func(input string) error {
+								Fail("should not be called")
+								return nil
+							})
+						})
+					})
+				})
+
+				Context("with a passing check", func() {
+					BeforeEach(func() {
+						checkErr = nil
+					})
+
+					It("should not return an error", func() {
+						_, err := actor.GetInputWithDefault(message, def, check)
+						Expect(err).NotTo(HaveOccurred())
+					})
+				})
+			})
+		})
+	})
+
+	Describe("GetInputWithDefaultAndRetry", func() {
+		var def = "default value"
+		Context("without any checks", func() {
+			BeforeEach(func() {
+				userInput = " user-input \n"
+			})
+
+			It("should return the trimmed input (just as without retry)", func() {
+				input, err := actor.GetInputWithDefaultAndRetry(message, def)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(input).To(Equal("user-input"))
+				Eventually(output).Should(gbytes.Say(`Please answer: `))
+			})
+		})
+
+		Context("with a check that fails the first time", func() {
+			var (
+				checkErr = errors.New("The first time fails!")
+				check    interact.InputCheck
+				i        int
+			)
+			BeforeEach(func() {
+				i = 0
+				check = func(input string) error {
+					if i == 0 {
+						Expect(input).To(Equal("user-input1"))
+						i++
+						return checkErr
+					}
+					Expect(input).To(Equal("correct-input"))
+					return nil
+				}
+			})
+
+			Context("with user retrying", func() {
+				BeforeEach(func() {
+					userInput = "user-input1\ny\ncorrect-input\n"
+				})
+
+				It("should return the second (correct) input", func() {
+					input, err := actor.GetInputWithDefaultAndRetry(message, def, check)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(input).To(Equal("correct-input"))
+				})
+
+				It("should have correct prompts", func() {
+					actor.GetInputWithDefaultAndRetry(message, def, check)
+					Eventually(output).Should(gbytes.Say(`Please answer: \(default value\) `))
+					Eventually(output).Should(gbytes.Say(`The first time fails!`))
+					Eventually(output).Should(gbytes.Say(`Do you want to try again\? \[y/N\]: `))
+					Eventually(output).Should(gbytes.Say(`Please answer: \(default value\) `))
+				})
+			})
+		})
+	})
 })
