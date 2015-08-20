9049eaff8846b6c25281c747555172826ce9bf3c
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-08-21 00:16:32 +0300

    Add PUT endpoint for offer group posts

--- handler/offer_group_post_handler.go
+++ handler/offer_group_post_handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 
 	"github.com/Lunchr/luncher-api/db"
 	"github.com/Lunchr/luncher-api/db/model"
@@ -44,6 +45,26 @@ func PostOfferGroupPost(c db.OfferGroupPosts, sessionManager session.Manager, us
 	return forRestaurant(sessionManager, users, restaurants, handler)
 }
 
+// PutOfferGroupPost handles PUT requests to /restaurant/posts/:date. It stores the info in the DB and updates the post in FB.
+func PutOfferGroupPost(c db.OfferGroupPosts, sessionManager session.Manager, users db.Users, restaurants db.Restaurants) router.HandlerWithParams {
+	handler := func(w http.ResponseWriter, r *http.Request, user *model.User, restaurant *model.Restaurant,
+		date model.DateWithoutTime) *router.HandlerError {
+		post, handlerErr := parseOfferGroupPost(r, restaurant)
+		if handlerErr != nil {
+			return handlerErr
+		}
+		if post.Date != date {
+			return router.NewSimpleHandlerError("Unexpected date value", http.StatusBadRequest)
+		}
+		err := c.UpdateByID(post.ID, post)
+		if err != nil {
+			return router.NewSimpleHandlerError("Failed to insert the post to DB", http.StatusBadRequest)
+		}
+		return writeJSON(w, post)
+	}
+	return forDate(sessionManager, users, restaurants, handler)
+}
+
 type HandlerWithRestaurantAndDate func(w http.ResponseWriter, r *http.Request, user *model.User, restaurant *model.Restaurant,
 	date model.DateWithoutTime) *router.HandlerError
 
@@ -65,8 +86,9 @@ func forDate(sessionManager session.Manager, users db.Users, restaurants db.Rest
 
 func parseOfferGroupPost(r *http.Request, restaurant *model.Restaurant) (*model.OfferGroupPost, *router.HandlerError) {
 	var post struct {
-		MessageTemplate string `json:"message_template"`
-		Date            string `json:"date"`
+		ID              bson.ObjectId `json:"_id"`
+		MessageTemplate string        `json:"message_template"`
+		Date            string        `json:"date"`
 	}
 	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
@@ -80,6 +102,7 @@ func parseOfferGroupPost(r *http.Request, restaurant *model.Restaurant) (*model.
 		return nil, router.NewSimpleHandlerError("Invalid date specified", http.StatusBadRequest)
 	}
 	return &model.OfferGroupPost{
+		ID:              post.ID,
 		MessageTemplate: post.MessageTemplate,
 		Date:            date,
 		RestaurantID:    restaurant.ID,
--- handler/offer_group_post_handler_test.go
+++ handler/offer_group_post_handler_test.go
@@ -230,7 +230,7 @@ var _ = Describe("OfferGroupPostHandlers", func() {
 						Expect(contentTypes[0]).To(Equal("application/json"))
 					})
 
-					It("should include the restaurant with the new ID", func() {
+					It("should include the post with the new ID", func() {
 						handler(responseRecorder, request)
 						var post *model.OfferGroupPost
 						json.Unmarshal(responseRecorder.Body.Bytes(), &post)
@@ -259,6 +259,155 @@ var _ = Describe("OfferGroupPostHandlers", func() {
 
 				It("should fail", func() {
 					err := handler(responseRecorder, request)
+					Expect(err).NotTo(BeNil())
+				})
+			})
+		})
+	})
+
+	Describe("PUT /restaurant/posts/:date", func() {
+		var (
+			sessionManager        session.Manager
+			postsCollection       db.OfferGroupPosts
+			restaurantsCollection db.Restaurants
+			usersCollection       db.Users
+			handler               router.HandlerWithParams
+		)
+
+		JustBeforeEach(func() {
+			handler = PutOfferGroupPost(postsCollection, sessionManager, usersCollection, restaurantsCollection)
+		})
+
+		ExpectUserToBeLoggedIn(func() *router.HandlerError {
+			return handler(responseRecorder, request, nil)
+		}, func(mgr session.Manager, users db.Users) {
+			sessionManager = mgr
+			usersCollection = users
+		})
+
+		Context("with session set and a matching user in DB", func() {
+			var (
+				mockSessionManager        *mocks.Manager
+				mockPostsCollection       *mocks.OfferGroupPosts
+				mockRestaurantsCollection *mocks.Restaurants
+				mockUsersCollection       *mocks.Users
+				restaurantID              bson.ObjectId
+				id                        bson.ObjectId
+				params                    httprouter.Params
+			)
+
+			BeforeEach(func() {
+				mockSessionManager = new(mocks.Manager)
+				sessionManager = mockSessionManager
+				mockPostsCollection = new(mocks.OfferGroupPosts)
+				postsCollection = mockPostsCollection
+				mockRestaurantsCollection = new(mocks.Restaurants)
+				restaurantsCollection = mockRestaurantsCollection
+				mockUsersCollection = new(mocks.Users)
+				usersCollection = mockUsersCollection
+				id = bson.NewObjectId()
+
+				restaurantID = bson.NewObjectId()
+				restaurant := &model.Restaurant{
+					ID: restaurantID,
+				}
+				user := &model.User{
+					RestaurantIDs: []bson.ObjectId{restaurant.ID},
+				}
+
+				mockSessionManager.On("Get", mock.Anything).Return("session", nil)
+				mockUsersCollection.On("GetSessionID", "session").Return(user, nil)
+				mockRestaurantsCollection.On("GetID", restaurantID).Return(restaurant, nil)
+
+				requestMethod = "PUT"
+				params = httprouter.Params{httprouter.Param{
+					Key:   "date",
+					Value: "2015-04-10",
+				}}
+			})
+
+			AfterEach(func() {
+				mockSessionManager.AssertExpectations(GinkgoT())
+				mockPostsCollection.AssertExpectations(GinkgoT())
+				mockRestaurantsCollection.AssertExpectations(GinkgoT())
+				mockUsersCollection.AssertExpectations(GinkgoT())
+			})
+
+			Context("with valid input", func() {
+				BeforeEach(func() {
+					requestData = map[string]interface{}{
+						"_id":              id,
+						"date":             "2015-04-10",
+						"message_template": "a message template",
+					}
+				})
+
+				Context("with DB update succeeding", func() {
+					BeforeEach(func() {
+						mockPostsCollection.On("UpdateByID", id, &model.OfferGroupPost{
+							ID:              id,
+							Date:            "2015-04-10",
+							RestaurantID:    restaurantID,
+							MessageTemplate: "a message template",
+						}).Return(nil)
+					})
+
+					It("should succeed", func() {
+						err := handler(responseRecorder, request, params)
+						Expect(err).To(BeNil())
+					})
+
+					It("should return json", func() {
+						handler(responseRecorder, request, params)
+						contentTypes := responseRecorder.HeaderMap["Content-Type"]
+						Expect(contentTypes).To(HaveLen(1))
+						Expect(contentTypes[0]).To(Equal("application/json"))
+					})
+
+					It("should return the post", func() {
+						handler(responseRecorder, request, params)
+						var post *model.OfferGroupPost
+						json.Unmarshal(responseRecorder.Body.Bytes(), &post)
+						Expect(post.ID).To(Equal(id))
+					})
+				})
+
+				Context("with DB update failing", func() {
+					BeforeEach(func() {
+						mockPostsCollection.On("UpdateByID", id, mock.AnythingOfType("*model.OfferGroupPost")).Return(errors.New("things"))
+					})
+
+					It("should fail", func() {
+						err := handler(responseRecorder, request, params)
+						Expect(err).NotTo(BeNil())
+					})
+				})
+			})
+
+			Context("with a non-matching date", func() {
+				BeforeEach(func() {
+					requestData = map[string]interface{}{
+						"_id":  id,
+						"date": "2015-04-11",
+					}
+				})
+
+				It("should fail", func() {
+					err := handler(responseRecorder, request, params)
+					Expect(err).NotTo(BeNil())
+				})
+			})
+
+			Context("without an id", func() {
+				BeforeEach(func() {
+					requestData = map[string]interface{}{
+						"date": "2015-04-10",
+					}
+					mockPostsCollection.On("UpdateByID", bson.ObjectId(""), mock.AnythingOfType("*model.OfferGroupPost")).Return(errors.New("missing stuff"))
+				})
+
+				It("should fail", func() {
+					err := handler(responseRecorder, request, params)
 					Expect(err).NotTo(BeNil())
 				})
 			})
