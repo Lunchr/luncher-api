e46d288000a3492174f1722065f7f4981ed251d2
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-08-20 18:43:41 +0300

    Add a way to get a group for date for restaurant

--- db/offer_group_posts.go
+++ db/offer_group_posts.go
@@ -10,6 +10,7 @@ type OfferGroupPosts interface {
 	Insert(...*model.OfferGroupPost) ([]*model.OfferGroupPost, error)
 	UpdateByID(bson.ObjectId, *model.OfferGroupPost) error
 	GetByID(bson.ObjectId) (*model.OfferGroupPost, error)
+	GetByDate(model.DateWithoutTime, bson.ObjectId) (*model.OfferGroupPost, error)
 }
 
 type offerGroupPostCollection struct {
@@ -41,5 +42,14 @@ func (c offerGroupPostCollection) UpdateByID(id bson.ObjectId, post *model.Offer
 func (c offerGroupPostCollection) GetByID(id bson.ObjectId) (*model.OfferGroupPost, error) {
 	var post model.OfferGroupPost
 	err := c.FindId(id).One(&post)
+	return &post, err
+}
+
+func (c offerGroupPostCollection) GetByDate(date model.DateWithoutTime, restaurantID bson.ObjectId) (*model.OfferGroupPost, error) {
+	var post model.OfferGroupPost
+	err := c.Find(bson.M{
+		"date":          date,
+		"restaurant_id": restaurantID,
+	}).One(&post)
 	return &post, err
 }
--- db/offer_group_posts_test.go
+++ db/offer_group_posts_test.go
@@ -1,6 +1,8 @@
 package db_test
 
 import (
+	"time"
+
 	"github.com/Lunchr/luncher-api/db/model"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -62,6 +64,33 @@ var _ = Describe("OfferGroupPosts", func() {
 				result, err := offerGroupPostsCollection.GetByID(id)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(result.MessageTemplate).To(Equal("an updated message"))
+			})
+		})
+	})
+
+	Describe("GetByDate", func() {
+		var date = model.DateFromTime(time.Date(2115, 04, 03, 0, 0, 0, 0, time.UTC))
+		var restaurantID = bson.NewObjectId()
+		RebuildDBAfterEach()
+
+		It("fails if not found", func() {
+			_, err := offerGroupPostsCollection.GetByDate(date, restaurantID)
+			Expect(err).To(HaveOccurred())
+		})
+
+		Context("with an post with known ID inserted", func() {
+			BeforeEach(func() {
+				post := aPost()
+				post.Date = date
+				post.RestaurantID = restaurantID
+				_, err := offerGroupPostsCollection.Insert(post)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("returns the found post", func() {
+				result, err := offerGroupPostsCollection.GetByDate(date, restaurantID)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(result).NotTo(BeNil())
 			})
 		})
 	})
