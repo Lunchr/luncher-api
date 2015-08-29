d3ed0c69767c5f76fb00a585a0c0fc2869253dd2
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-08-29 12:27:10 +0200

    Improve token generation tests

--- db/model/registration_access_token_test.go
+++ db/model/registration_access_token_test.go
@@ -20,9 +20,18 @@ var _ = Describe("RegistrationAccessToken", func() {
 		})
 
 		Describe("TokenFromString", func() {
-			It("creates a correct token from a known string and back", func() {
+			It("creates a correct token from a known string", func() {
 				t, err := model.TokenFromString("EF4120DA-0302-BCEE-712B-1C258D2FB6D4")
 				Expect(err).NotTo(HaveOccurred())
+				Expect(t).To(Equal(model.Token{0xef, 0x41, 0x20, 0xda, 0x3, 0x2, 0xbc, 0xee, 0x71, 0x2b,
+					0x1c, 0x25, 0x8d, 0x2f, 0xb6, 0xd4}))
+			})
+		})
+
+		Describe("String", func() {
+			It("creates a correct string from known token", func() {
+				t := model.Token{0xef, 0x41, 0x20, 0xda, 0x3, 0x2, 0xbc, 0xee, 0x71, 0x2b, 0x1c, 0x25, 0x8d,
+					0x2f, 0xb6, 0xd4}
 				Expect(t.String()).To(Equal("EF4120DA-0302-BCEE-712B-1C258D2FB6D4"))
 			})
 		})
