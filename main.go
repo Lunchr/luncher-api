9b7c977ebd9a7668d831214949cff1684830ea88
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2016-04-12 23:10:00 +0300

    Format main router to use multiline statements
    Still not as readable as I'd like, but probably better.

--- main.go
+++ main.go
@@ -60,35 +60,101 @@ func main() {
 		facebookLoginAuthenticator, imageStorage, collageLayout)
 
 	r := router.NewWithPrefix("/api/v1/")
-	r.GET("/regions", handler.Regions(regionsCollection))
-	r.GETWithParams("/regions/:name/offers", handler.RegionOffers(offersCollection, regionsCollection, imageStorage))
-	r.GET("/offers", handler.ProximalOffers(offersCollection, imageStorage))
-	r.POSTWithParams("/restaurants/:restaurantID/offers", handler.PostOffers(offersCollection, usersCollection, restaurantsCollection, sessionManager,
-		imageStorage, facebookPost, regionsCollection))
-	r.PUT("/restaurants/:restaurantID/offers/:id", handler.PutOffers(offersCollection, usersCollection, restaurantsCollection, sessionManager,
-		imageStorage, facebookPost, regionsCollection))
-	r.DELETE("/restaurants/:restaurantID/offers/:id", handler.DeleteOffers(offersCollection, usersCollection, sessionManager, restaurantsCollection,
-		facebookPost, regionsCollection))
-	r.GET("/tags", handler.Tags(tagsCollection))
-	r.GET("/user/restaurants", handler.UserRestaurants(restaurantsCollection, sessionManager, usersCollection))
-	r.GETWithParams("/restaurants/:restaurantID", handler.Restaurant(restaurantsCollection, sessionManager, usersCollection))
-	r.POST("/restaurants", handler.PostRestaurants(restaurantsCollection, sessionManager, usersCollection, facebookLoginAuthenticator))
-	r.GETWithParams("/restaurants/:restaurantID/offers", handler.RestaurantOffers(restaurantsCollection, sessionManager, usersCollection,
-		offersCollection, imageStorage, regionsCollection))
-	r.POSTWithParams("/restaurants/:restaurantID/offer_suggestions", handler.RestaurantOfferSuggestions(restaurantsCollection, sessionManager, usersCollection,
-		offersCollection))
-	r.GETWithParams("/restaurants/:restaurantID/posts/:date", handler.OfferGroupPost(offerGroupPostsCollection, sessionManager, usersCollection, restaurantsCollection))
-	r.POSTWithParams("/restaurants/:restaurantID/posts", handler.PostOfferGroupPost(offerGroupPostsCollection, sessionManager, usersCollection,
-		restaurantsCollection, facebookPost))
-	r.PUT("/restaurants/:restaurantID/posts/:date", handler.PutOfferGroupPost(offerGroupPostsCollection, sessionManager, usersCollection,
-		restaurantsCollection, facebookPost))
-	r.GET("/logout", handler.Logout(sessionManager, usersCollection))
-	r.GET("/login/facebook", handler.RedirectToFBForLogin(sessionManager, facebookLoginAuthenticator))
-	r.GET("/login/facebook/redirected", handler.RedirectedFromFBForLogin(sessionManager, facebookLoginAuthenticator, usersCollection, restaurantsCollection))
-	r.GET("/register/facebook", handler.RedirectToFBForRegistration(sessionManager, facebookRegistrationAuthenticator, registrationTokensCollection))
-	r.GET("/register/facebook/redirected", handler.RedirectedFromFBForRegistration(sessionManager, facebookRegistrationAuthenticator, usersCollection))
-	r.GET("/register/facebook/pages", handler.ListPagesManagedByUser(sessionManager, facebookLoginAuthenticator, usersCollection))
-	r.GETWithParams("/register/facebook/pages/:id", handler.Page(sessionManager, facebookLoginAuthenticator, usersCollection))
+	r.GET(
+		"/regions",
+		handler.Regions(regionsCollection),
+	)
+	r.GETWithParams(
+		"/regions/:name/offers",
+		handler.RegionOffers(offersCollection, regionsCollection, imageStorage),
+	)
+	r.GET(
+		"/offers",
+		handler.ProximalOffers(offersCollection, imageStorage),
+	)
+	r.POSTWithParams(
+		"/restaurants/:restaurantID/offers",
+		handler.PostOffers(offersCollection, usersCollection, restaurantsCollection, sessionManager, imageStorage,
+			facebookPost, regionsCollection),
+	)
+	r.PUT(
+		"/restaurants/:restaurantID/offers/:id",
+		handler.PutOffers(offersCollection, usersCollection, restaurantsCollection, sessionManager, imageStorage,
+			facebookPost, regionsCollection),
+	)
+	r.DELETE(
+		"/restaurants/:restaurantID/offers/:id",
+		handler.DeleteOffers(offersCollection, usersCollection, sessionManager, restaurantsCollection, facebookPost,
+			regionsCollection),
+	)
+	r.GET(
+		"/tags",
+		handler.Tags(tagsCollection),
+	)
+	r.GET(
+		"/user/restaurants",
+		handler.UserRestaurants(restaurantsCollection, sessionManager, usersCollection),
+	)
+	r.GETWithParams(
+		"/restaurants/:restaurantID",
+		handler.Restaurant(restaurantsCollection, sessionManager, usersCollection),
+	)
+	r.POST(
+		"/restaurants",
+		handler.PostRestaurants(restaurantsCollection, sessionManager, usersCollection, facebookLoginAuthenticator),
+	)
+	r.GETWithParams(
+		"/restaurants/:restaurantID/offers",
+		handler.RestaurantOffers(restaurantsCollection, sessionManager, usersCollection, offersCollection,
+			imageStorage, regionsCollection),
+	)
+	r.POSTWithParams(
+		"/restaurants/:restaurantID/offer_suggestions",
+		handler.RestaurantOfferSuggestions(restaurantsCollection, sessionManager, usersCollection, offersCollection),
+	)
+	r.GETWithParams(
+		"/restaurants/:restaurantID/posts/:date",
+		handler.OfferGroupPost(offerGroupPostsCollection, sessionManager, usersCollection, restaurantsCollection),
+	)
+	r.POSTWithParams(
+		"/restaurants/:restaurantID/posts",
+		handler.PostOfferGroupPost(offerGroupPostsCollection, sessionManager, usersCollection, restaurantsCollection,
+			facebookPost),
+	)
+	r.PUT(
+		"/restaurants/:restaurantID/posts/:date",
+		handler.PutOfferGroupPost(offerGroupPostsCollection, sessionManager, usersCollection, restaurantsCollection,
+			facebookPost),
+	)
+	r.GET(
+		"/logout",
+		handler.Logout(sessionManager, usersCollection),
+	)
+	r.GET(
+		"/login/facebook", handler.RedirectToFBForLogin(sessionManager, facebookLoginAuthenticator),
+	)
+	r.GET(
+		"/login/facebook/redirected",
+		handler.RedirectedFromFBForLogin(sessionManager, facebookLoginAuthenticator, usersCollection,
+			restaurantsCollection),
+	)
+	r.GET(
+		"/register/facebook",
+		handler.RedirectToFBForRegistration(sessionManager, facebookRegistrationAuthenticator,
+			registrationTokensCollection),
+	)
+	r.GET(
+		"/register/facebook/redirected",
+		handler.RedirectedFromFBForRegistration(sessionManager, facebookRegistrationAuthenticator, usersCollection),
+	)
+	r.GET(
+		"/register/facebook/pages",
+		handler.ListPagesManagedByUser(sessionManager, facebookLoginAuthenticator, usersCollection),
+	)
+	r.GETWithParams(
+		"/register/facebook/pages/:id",
+		handler.Page(sessionManager, facebookLoginAuthenticator, usersCollection),
+	)
 
 	http.Handle("/api/v1/", r)
 	portString := fmt.Sprintf(":%d", mainConfig.Port)
